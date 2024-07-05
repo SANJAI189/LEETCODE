c03dd1fbc614b64da99781661936c0150c0a80c1
SANJAI <148245422+SANJAI189@users.noreply.github.com>
2024-07-05 12:22:20 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (87.61%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -1,16 +1,35 @@
+// class Solution {
+//   public int minimumPushes(String word) {
+//      int n=word.length();
+//      if(n<=8){
+//         return n;
+//      }
+//      int sum=0;
+//      int i=1;
+//      while(n>8){
+//         n=n-8;
+//         sum=sum+i*8;
+//         i++;
+//      }
+//      return sum+n*i;
+//     }
+// }
+
+
+
 class Solution {
-  public int minimumPushes(String word) {
-     int n=word.length();
-     if(n<=8){
-        return n;
-     }
-     int sum=0;
-     int i=1;
-     while(n>8){
-        n=n-8;
-        sum=sum+i*8;
-        i++;
-     }
-     return sum+n*i;
+    public int minimumPushes(String word) {
+        int count=0;
+        if(word.length()/8==0){
+            count=word.length()%8;
+        }else if(word.length()/8==1){
+            count=8+(word.length()%8)*2;
+        }
+        else if(word.length()/8==2){
+            count=24+(word.length()%8)*3;
+        }else {
+            count=48+(word.length()%8)*4;
+        }
+        return count;
     }
 }
