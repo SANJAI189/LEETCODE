c9769b3b4f5f2e53840c3c2ace0c7ab58d4b844e
SANJAI <148245422+SANJAI189@users.noreply.github.com>
2024-08-27 23:38:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (5.03%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,16 +1,41 @@
+// optimal solution
+//Transpose the matrix and reverse the row;
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        int[][] image = new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                image[j][n-1-i] = matrix[i][j];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<i;j++){
+                //swap
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j] = image[i][j];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length/2;j++){
+                //swap
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j]=temp;
             }
         }
     }
 }
+
+
+// Brute - Force approach
+// class Solution {
+//     public void rotate(int[][] matrix) {
+//         int n = matrix.length;
+//         int[][] image = new int[n][n];
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 image[j][n-1-i] = matrix[i][j];
+//             }
+//         }
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 matrix[i][j] = image[i][j];
+//             }
+//         }
+//     }
+// }
